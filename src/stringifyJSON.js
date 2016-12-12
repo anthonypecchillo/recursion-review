9465b3d0f853a4d3e0a3e5797fb3befdd0b07d03
anthonypecchillo <anthonypecchillo@gmail.com>
2016-12-12 15:11:21 -0800

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result;
+  // Check for unstringifiable values first
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+    result = '';
+  // Check if we have an array
+  } else if (Array.isArray(obj)) {
+    result = '[';
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]);
+      result += (i < obj.length - 1) ? ',' : '';
+    }
+    result += ']';
+  // Check for null
+  } else if (obj === null) {
+    result = 'null';
+  // Check if we have an object
+  } else if (typeof obj === 'object') {
+    result = '{';
+    var count = 0;
+    for (var key in obj) {
+      if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';  
+        count++;
+      }
+    }
+    if (count !== 0) {
+      result = result.substring(0, result.length - 1);
+    }
+    result += '}';
+  } else if (typeof obj === 'string') {
+    result = '"' + obj + '"';
+  // Everything else can be handled here
+  } else {
+    result = obj.toString();
+  }
+
+  return result;
+
 };
